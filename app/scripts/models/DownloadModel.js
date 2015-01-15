47611cc3dcd51e8e1378a5a93a1a5d3915098514
martin.paces <martin.paces@eox.at>
2015-01-15 15:27:11 +0100

    Code cleanup.

--- app/scripts/models/DownloadModel.js
+++ app/scripts/models/DownloadModel.js
@@ -9,8 +9,8 @@
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-// copies of the Software, and to permit persons to whom the Software is 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
 // The above copyright notice and this permission notice shall be included in all
@@ -26,16 +26,15 @@
 //-------------------------------------------------------------------------------
 
 (function() {
-	'use strict';
-	var root = this;
-	root.define(['backbone','communicator'],
-	function( Backbone, Communicator ) {
-		var DownloadModel = Backbone.Model.extend({
-			ToI: null,		// Time of interes
-			AoI: null, 		// Area of Interest
-			products: {} 	// Selected products
-		
-		});
-		return {'DownloadModel':DownloadModel};
-	});
-}).call( this );
+    'use strict';
+    var root = this;
+    root.define(['backbone','communicator'],
+    function(Backbone, Communicator){
+        var DownloadModel = Backbone.Model.extend({
+            ToI: null,      // Time of Interest
+            AoI: null,      // Area of Interest
+            products: {}    // Selected products
+        });
+        return {'DownloadModel':DownloadModel};
+    });
+}).call(this);
--- app/scripts/views/DownloadSelectionView.js
+++ app/scripts/views/DownloadSelectionView.js
@@ -1,49 +1,41 @@
 (function() {
-	'use strict';
+    'use strict';
+    var root = this;
+    root.define([
+        'backbone',
+        'communicator',
+        'hbs!tmpl/DownloadSelection',
+        'underscore',
+        "bootstrap-datepicker"
+    ],
+    function(Backbone, Communicator, DownloadSelectionTmpl){
+        var DownloadSelectionView = Backbone.Marionette.CompositeView.extend({
 
-	var root = this;
+            tagName: "div",
+            className: "panel panel-default downloadselection not-selectable",
+            template: {type: 'handlebars', template: DownloadSelectionTmpl},
+            events: {
+                'click #btn-draw-bbox':'onBBoxClick',
+                "click #btn-clear-bbox" : 'onClearClick',
+                "click #btn-download" : 'onDownloadClick',
+                "change #txt-minx" : "onBBoxChange",
+                "change #txt-maxx" : "onBBoxChange",
+                "change #txt-miny" : "onBBoxChange",
+                "change #txt-maxy" : "onBBoxChange",
+                "hide": "onCloseTimeWidget"
+            },
 
-	root.define([
-		'backbone',
-		'communicator',
-		'hbs!tmpl/DownloadSelection',
-		'underscore',
-		"bootstrap-datepicker"
-	],
-
-	function( Backbone, Communicator, DownloadSelectionTmpl ) {
-
-		var DownloadSelectionView = Backbone.Marionette.CompositeView.extend({  
-
-			tagName: "div",
-			className: "panel panel-default downloadselection not-selectable",
-			template: {type: 'handlebars', template: DownloadSelectionTmpl},
-			events: {
-				'click #btn-draw-bbox':'onBBoxClick',
-				"click #btn-clear-bbox" : 'onClearClick',
-				"click #btn-download" : 'onDownloadClick',
-				"change #txt-minx" : "onBBoxChange",
-				"change #txt-maxx" : "onBBoxChange",
-				"change #txt-miny" : "onBBoxChange",
-				"change #txt-maxy" : "onBBoxChange",
-				"hide": "onCloseTimeWidget"
-			},
-			
-			onShow: function (view) {
-
-				this.listenTo(Communicator.mediator, 'time:change', this.onTimeChange);
-	        	this.listenTo(Communicator.mediator, "selection:changed", this.onSelectionChange);
-
-	        	this.timeinterval = {};
-
-	        	this.delegateEvents(this.events);
-
-				this.$('.close').on("click", _.bind(this.onClose, this));
-				this.$el.draggable({ 
-		    		containment: "#content" ,
-		    		scroll: false,
-		    		handle: '.panel-heading'
-		    	});
+            onShow: function (view) {
+                this.listenTo(Communicator.mediator, 'time:change', this.onTimeChange);
+                this.listenTo(Communicator.mediator, "selection:changed", this.onSelectionChange);
+                this.timeinterval = {};
+                this.delegateEvents(this.events);
+                this.$('.close').on("click", _.bind(this.onClose, this));
+                this.$el.draggable({
+                    containment: "#content" ,
+                    scroll: false,
+                    handle: '.panel-heading'
+                });
 
                 var aoi = this.model.get("AoI")
                 if (aoi){
@@ -56,98 +48,90 @@
                     this.$("#btn-download").attr("disabled", "disabled");
                 }
 
-		    	this.$('#div-date-begin input[type="text"]').datepicker({autoclose: true, format: "dd/mm/yyyy", keyboardNavigation: false});
-		    	this.$('#div-date-begin input[type="text"]').datepicker('update', this.model.get('ToI').start);
-		    	this.$('#div-date-begin input[type="text"]').datepicker('setDate', this.model.get('ToI').start);
+                this.$('#div-date-begin input[type="text"]').datepicker({autoclose: true, format: "yyyy-mm-dd", keyboardNavigation: false});
+                this.$('#div-date-begin input[type="text"]').datepicker('update', this.model.get('ToI').start);
+                this.$('#div-date-begin input[type="text"]').datepicker('setDate', this.model.get('ToI').start);
 
-		    	this.$('#div-date-end input[type="text"]').datepicker({autoclose: true, format: "dd/mm/yyyy"});
-		    	this.$('#div-date-end input[type="text"]').datepicker('update', this.model.get('ToI').end);
+                this.$('#div-date-end input[type="text"]').datepicker({autoclose: true, format: "yyyy-mm-dd"});
+                this.$('#div-date-end input[type="text"]').datepicker('update', this.model.get('ToI').end);
+                this.$('#div-date-end input[type="text"]').datepicker('setDate', this.model.get('ToI').end);
 
-				$(document).on('touch click', '#div-date-begin .input-group-addon', function(e){
-				    $('input[type="text"]', $(this).parent()).focus();
-				});
-				$(document).on('touch click', '#div-date-end .input-group-addon', function(e){
-				    $('input[type="text"]', $(this).parent()).focus();
-				});
+                $(document).on('touch click', '#div-date-begin .input-group-addon', function(e){
+                    $('input[type="text"]', $(this).parent()).focus();
+                });
+                $(document).on('touch click', '#div-date-end .input-group-addon', function(e){
+                    $('input[type="text"]', $(this).parent()).focus();
+                });
+            },
 
-			},
+            onBBoxClick: function() {
+                $("#txt-minx").val("");
+                $("#txt-maxx").val("");
+                $("#txt-miny").val("");
+                $("#txt-maxy").val("");
+                Communicator.mediator.trigger('selection:activated',{id:"bboxSelection",active:true});
+            },
 
-			onBBoxClick: function() {
-				$("#txt-minx").val("");
-				$("#txt-maxx").val("");
-				$("#txt-miny").val("");
-				$("#txt-maxy").val("");
-				Communicator.mediator.trigger('selection:activated',{id:"bboxSelection",active:true});
-			},
-
-			onClearClick: function () {
-				Communicator.mediator.trigger('selection:activated',{id:"bboxSelection",active:false});
-				$("#txt-minx").val("");
-				$("#txt-maxx").val("");
-				$("#txt-miny").val("");
-				$("#txt-maxy").val("");
+            onClearClick: function () {
+                Communicator.mediator.trigger('selection:activated',{id:"bboxSelection",active:false});
+                $("#txt-minx").val("");
+                $("#txt-maxx").val("");
+                $("#txt-miny").val("");
+                $("#txt-maxy").val("");
                 this.$("#btn-download").attr("disabled", "disabled");
-			},
+            },
 
-			onDownloadClick: function () {
-				Communicator.mediator.trigger("dialog:open:download", true);
-			},
+            onDownloadClick: function () {
+                Communicator.mediator.trigger("dialog:open:download", true);
+            },
 
-			onTimeChange: function (time) {
-		    	this.$('#div-date-begin input[type="text"]').datepicker('update', this.model.get('ToI').start);
-		    	this.$('#div-date-end input[type="text"]').datepicker('update', this.model.get('ToI').end);
-			},
+            onTimeChange: function (time) {
+                this.$('#div-date-begin input[type="text"]').datepicker('update', this.model.get('ToI').start);
+                this.$('#div-date-end input[type="text"]').datepicker('update', this.model.get('ToI').end);
+            },
 
+            onCloseTimeWidget: function(evt){
+                var opt = {
+                    start: this.$('#div-date-begin input[type="text"]').datepicker('getDate'),
+                    end: this.$('#div-date-end input[type="text"]').datepicker('getDate')
+                };
+                Communicator.mediator.trigger('date:selection:change', opt);
+            },
 
-			onCloseTimeWidget: function(evt){
-				var opt = {
-					start: this.$('#div-date-begin input[type="text"]').datepicker('getDate'),
-					end: this.$('#div-date-end input[type="text"]').datepicker('getDate')
-				};
-				Communicator.mediator.trigger('date:selection:change', opt);
-			},
-
-			onSelectionChange: function (obj) {
-				if (obj){
-					$("#txt-minx").val(obj.bounds.left);
-					$("#txt-maxx").val(obj.bounds.right);
-					$("#txt-miny").val(obj.bounds.bottom);
-					$("#txt-maxy").val(obj.bounds.top);
+            onSelectionChange: function (obj) {
+                if (obj){
+                    $("#txt-minx").val(obj.bounds.left);
+                    $("#txt-maxx").val(obj.bounds.right);
+                    $("#txt-miny").val(obj.bounds.bottom);
+                    $("#txt-maxy").val(obj.bounds.top);
                     this.$("#btn-download").removeAttr("disabled");
                 } else {
                     this.$("#btn-download").attr("disabled", "disabled");
-				}
-				
-			},
+                }
+            },
 
-			onBBoxChange: function (event) {
-
-				var values = {
-					left: parseFloat($("#txt-minx").val()),
-					right: parseFloat($("#txt-maxx").val()),
-					bottom: parseFloat($("#txt-miny").val()),
-					top: parseFloat($("#txt-maxy").val())
-				};
-
+            onBBoxChange: function (event) {
+                var values = {
+                    left: parseFloat($("#txt-minx").val()),
+                    right: parseFloat($("#txt-maxx").val()),
+                    bottom: parseFloat($("#txt-miny").val()),
+                    top: parseFloat($("#txt-maxy").val())
+                };
                 this.$("#btn-download").attr("disabled", "disabled");
-
-				if(!isNaN(values.left) && !isNaN(values.right) &&
-					!isNaN(values.bottom) && !isNaN(values.top) ) {
-					
-					if ( !(values.left > values.right || values.bottom > values.top)){
-						Communicator.mediator.trigger('selection:bbox:changed',values);
+                if(!isNaN(values.left) && !isNaN(values.right) && !isNaN(values.bottom) && !isNaN(values.top)) {
+                    if ( !(values.left > values.right || values.bottom > values.top)){
+                        Communicator.mediator.trigger('selection:bbox:changed',values);
                         this.$("#btn-download").removeAttr("disabled");
-					}
-				}
-			},
+                    }
+                }
+            },
 
+            onClose: function() {
+                this.close();
+            }
 
-			onClose: function() {
-				this.close();
-			}
-			
-		});
-		return {'DownloadSelectionView':DownloadSelectionView};
-	});
+        });
+        return {'DownloadSelectionView':DownloadSelectionView};
+    });
 
 }).call( this );
